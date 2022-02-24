f35e09622e127d0a8c4669e146c5f938a36410ca
ishubham326 <ishubham326@gmail.com>
2022-02-24 18:17:42 -0500

    updated pics, updated code to show score, and highscore (stores in local storage)

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 https://distracted-yalow-d7574c.netlify.app/
 
-My second react app. A simple tenzies game that I learned while following a course with several additions to the original project.
+My second react app. A simple tenzies game that I learned while following a course with several additions of my own to the original project. Current score display, as well as highscore being set on rolls
 
 ![screen shot](https://github.com/ishubham326/tenzies-react/blob/main/image-1.png)
 
--- src/App.js
+++ src/App.js
@@ -6,12 +6,22 @@ import Confetti from "react-confetti";
 export default function App() {
   const [dice, setDice] = React.useState(allNewDice());
   const [tenzies, setTenzies] = React.useState(false);
+  const [numRolls, setNumRolls] = React.useState(0);
+  const [highscore, setHighscore] = React.useState(
+    JSON.parse(localStorage.getItem("highscore") || 40)
+  );
 
   React.useEffect(() => {
     const allHeld = dice.every((die) => die.isHeld);
     const value = dice[0].value;
     const allValue = dice.every((die) => die.value === value);
-    if (allHeld && allValue) setTenzies(true);
+    if (allHeld && allValue) {
+      setTenzies(true);
+      if (numRolls <= highscore) {
+        setHighscore(numRolls);
+        localStorage.setItem("highscore", JSON.stringify(numRolls));
+      }
+    }
   }, [dice]);
 
   function allNewDice() {
@@ -34,6 +44,8 @@ export default function App() {
           : { ...die, value: Math.floor(Math.random() * 7) + 1, id: nanoid() };
       })
     );
+
+    setNumRolls((oldNum) => (oldNum = oldNum + 1));
   }
 
   function holdDice(id) {
@@ -47,7 +59,9 @@ export default function App() {
   function newGame() {
     setTenzies(false);
     setDice(allNewDice());
+    setNumRolls(0);
   }
+
   const diceElements = dice.map((die) => (
     <Die
       key={die.id}
@@ -66,6 +80,8 @@ export default function App() {
         Click each die to freeze it at its current value betweel rolls.
       </p>
       <div className="dice-container">{diceElements}</div>
+      <p>Number of Rolls: {numRolls}</p>
+      <p>Lowest Number of Rolls: {highscore}</p>
       <button onClick={tenzies ? newGame : rollDice} className="roll-dice">
         {tenzies ? "New Game" : "Roll"}
       </button>
